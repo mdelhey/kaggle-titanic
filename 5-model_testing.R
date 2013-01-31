ad937e056111b2cbe672d055793e239809d8aef1
Matt Delhey <matt.delhey@gmail.com>
2013-01-31 10:29:12 -0600

    Update 5-model_testing.R

--- 5-model_testing.R
+++ 5-model_testing.R
@@ -1,2 +1,14 @@
 # Goal: Test the accuracy of our model.
+library(plyr)
 
+###
+### Training Error
+###
+
+# The training error is the error in our model
+# on the TRAIN data set. This is usually NOT a 
+# good measure of model accuracy on new data.
+
+###
+### Cross Validation
+###
